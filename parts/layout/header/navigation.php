7f1091a4944fea7a365f6c5242b55dd7865a2bbe
nabin-nh <nabin.hussain@gmail.com>
2022-03-19 23:10:43 +0000

    Add dropdown menus to navigation

--- parts/layout/header/navigation.php
+++ parts/layout/header/navigation.php
@@ -31,28 +31,167 @@
                         <a class="nav-link" href="#">Home</a>
                     </li>
 
-                    <li>
-                        <a class="nav-link" href="#">About BFC</a>
+                    <li class="dropdown">
+
+                        <a 
+                            class="nav-link" 
+                            href="#"
+                            id="about"
+                            role="button" 
+                            data-bs-toggle="dropdown" 
+                            aria-expanded="false"
+                        >
+                            About BFC
+                            <i class="icon-dropdown-arrow"></i>
+                        </a>
+                            
+                        <ul class="dropdown-menu" aria-labelledby="about">
+                            <li>
+                                <a class="dropdown-item" href="#">About Tibbetts Group</a>
+                            </li>
+
+                            <li>
+                                <a class="dropdown-item" href="#">History</a>
+                            </li>
+
+                            <li>
+                                <a class="dropdown-item" href="#">Mission Statement & Values</a>
+                            </li>
+
+                            <li>
+                                <a class="dropdown-item" href="#">Charities</a>
+                            </li>
+
+                            <li>
+                                <a class="dropdown-item" href="#">Careers</a>
+                            </li>
+                        </ul>
+
                     </li>
 
-                    <li>
-                        <a class="nav-link" href="#">Industries</a>
+                    <li class="dropdown">
+
+                        <a 
+                            class="nav-link" 
+                            href="#"
+                            id="industries"
+                            role="button" 
+                            data-bs-toggle="dropdown" 
+                            aria-expanded="false"
+                        >
+                            Industries
+                            <i class="icon-dropdown-arrow"></i>
+                        </a>
+
+                        <ul class="dropdown-menu" aria-labelledby="industries">
+                            <li>
+                                <a class="dropdown-item" href="#">Industrial</a>
+                            </li>
+
+                            <li>
+                                <a class="dropdown-item" href="#">Aerospace</a>
+                            </li>
+
+                            <li>
+                                <a class="dropdown-item" href="#">Automotive</a>
+                            </li>
+                        </ul>
+
                     </li>
 
                     <li>
                         <a class="nav-link" href="#">BFC Aerospace</a>
                     </li>
 
-                    <li>
-                        <a class="nav-link" href="#">Products</a>
+                    <li class="dropdown">
+
+                        <a 
+                            class="nav-link" 
+                            href="#"
+                            id="products"
+                            role="button" 
+                            data-bs-toggle="dropdown" 
+                            aria-expanded="false"
+                        >
+                            Products
+                            <i class="icon-dropdown-arrow"></i>
+                        </a>
+                        
+                        <ul class="dropdown-menu" aria-labelledby="products">
+                            <li>
+                                <a class="dropdown-item" href="#">Associated Parts (ASP)</a>
+                            </li>
+
+                            <li>
+                                <a class="dropdown-item" href="#">Bespoke Components (BES)</a>
+                            </li>
+
+                            <li>
+                                <a class="dropdown-item" href="#">Standard Fasteners (STD)</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Standard Stainless Fasteners (STDSS)</a>
+                            </li>
+                        </ul>
+
                     </li>
 
-                    <li>
-                        <a class="nav-link" href="#">Quality</a>
+                    <li class="dropdown">
+
+                        <a 
+                            class="nav-link" 
+                            href="#"
+                            id="quality"
+                            role="button" 
+                            data-bs-toggle="dropdown" 
+                            aria-expanded="false"
+                        >
+                            Quality
+                            <i class="icon-dropdown-arrow"></i>
+                        </a>
+
+                        <ul class="dropdown-menu" aria-labelledby="quality">
+                            <li>
+                                <a class="dropdown-item" href="#">Quality certifications</a>
+                            </li>
+
+                            <li>
+                                <a class="dropdown-item" href="#">Engineering</a>
+                            </li>
+                        </ul>
+
                     </li>
 
-                    <li>
-                        <a class="nav-link" href="#">Documents</a>
+                    <li class="dropdown">
+
+                        <a 
+                            class="nav-link" 
+                            href="#"
+                            id="documents"
+                            role="button" 
+                            data-bs-toggle="dropdown" 
+                            aria-expanded="false"
+                        >
+                            Documents
+                            <i class="icon-dropdown-arrow"></i>
+                        </a>
+
+                        <ul class="dropdown-menu" aria-labelledby="documents">
+                            <li>
+                                <a class="dropdown-item" href="#">Catalogues</a>
+                            </li>
+
+                            <li>
+                                <a class="dropdown-item" href="#">Warranty</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Policy</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Admin</a>
+                            </li>
+                        </ul>
+
                     </li>
 
                     <li>
