73486ab215c3782fce590661c517191064eac595
nabin-nh <nabin.hussain@gmail.com>
2022-03-19 14:51:28 +0000

    Add main navigation and style it

---
+++ parts/layout/header/navigation.php
@@ -0,0 +1,71 @@
+<div class="navigation">
+    <div class="container-fluid">
+
+        <nav class="navbar navbar-expand-xl navbar-white">
+
+            <a class="navbar-brand" href="/">
+                <img 
+                    src="/assets/img/logos/bfc-security-logo.svg" 
+                    alt="BFC Security"
+                    class="img-fluid logo"
+                    width="192"
+                    height="87"
+                >
+            </a>
+
+            <button 
+                class="navbar-toggler bg-white" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#main-navigation" 
+                aria-controls="main-navigation" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+            >
+                <i class="icon-menu"></i>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-center" id="main-navigation">
+                <ul class="navbar-nav">
+                    <li>
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link" href="#">About BFC</a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link" href="#">Industries</a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link" href="#">BFC Aerospace</a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link" href="#">Products</a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link" href="#">Quality</a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link" href="#">Documents</a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link" href="#">News</a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+
+    </div>
+</div>
