c402dd44ca0c0eabd7e86949958c4ae88cc45db1
nabin-nh <nabin.hussain@gmail.com>
2022-03-21 22:50:28 +0000

    Start styling banner carousel

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php require_once 'header.php'; ?>
+
+    <?php 
+        require_once 'parts/flexible/banner-carousel.php'; 
+    ?>
+
 <?php require_once 'footer.php'; ?>
---
+++ parts/flexible/banner-carousel.php
@@ -0,0 +1,39 @@
+<section class="banner-carousel">
+    <div id="banner-indicators" class="carousel slide" data-bs-ride="carousel">
+
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#banner-indicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+
+            <button type="button" data-bs-target="#banner-indicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+
+            <button type="button" data-bs-target="#banner-indicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div
+                    class="image" 
+                    style="background-image: url('/assets/img/backgrounds/slide-one.jpg')"
+                >
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <div
+                    class="image" 
+                    style="background-image: url('/assets/img/backgrounds/slide-two.jpg')"
+                >
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <div
+                    class="image" 
+                    style="background-image: url('/assets/img/backgrounds/slide-three.jpg')"
+                >
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
