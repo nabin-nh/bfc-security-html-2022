d282a0052fac62e5a48660ead53804dd44664193
nabin-nh <nabin.hussain@gmail.com>
2022-04-03 14:49:02 +0100

    Create & style banner carousel

--- parts/flexible/banner-carousel.php
+++ parts/flexible/banner-carousel.php
@@ -1,20 +1,32 @@
 <section class="banner-carousel">
     <div id="banner-indicators" class="carousel slide" data-bs-ride="carousel">
 
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#banner-indicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-
-            <button type="button" data-bs-target="#banner-indicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-
-            <button type="button" data-bs-target="#banner-indicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <div
                     class="image" 
                     style="background-image: url('/assets/img/backgrounds/slide-one.jpg')"
+                    role="img"
+                    aria-label="A wide range of different bolts and screws"
                 >
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <div class="content">
+                                    <h4>Global Fastener Distributor</h4>
+
+                                    <h1>Engineering excellence is in our DNA</h1>
+
+                                    <p>BFC is a specialist UK supplier of fastener components to the global Automotive, Aerospace and Industrial markets with nearly 50 years experience.</p>
+
+                                    <a href="#" class="btn custom blue">Download BFC Catalogues</a>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
@@ -22,7 +34,25 @@
                 <div
                     class="image" 
                     style="background-image: url('/assets/img/backgrounds/slide-two.jpg')"
+                    role="img"
+                    aria-label="A range of silver screws"
                 >
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <div class="content">
+                                    <h4>You can trust our name and our products</h4>
+
+                                    <h1>WE supply fastener components you can rely on</h1>
+
+                                    <p>As well as standard fasteners, BFC offer Turned Parts, Pressings, Sintered Metal Parts, Seals and Rubber Product, Specialist Cold Formings, Die Castings and Sub-Assemblies.</p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
@@ -30,9 +60,35 @@
                 <div
                     class="image" 
                     style="background-image: url('/assets/img/backgrounds/slide-three.jpg')"
+                    role="img"
+                    aria-label="Outside view of the Tibbetts Group BFC building"
                 >
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <div class="content">
+                                    <h4>No compromise on specification or quality</h4>
+
+                                    <h1>WE understand our customers requirements and deliver</h1>
+
+                                    <p>BFC is a division of Tibbetts Group Ltd, which gives financial stability and operational support allowing sales revenue momentum with current and new customers.</p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
+        </div>
+
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#banner-indicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+
+            <button type="button" data-bs-target="#banner-indicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+
+            <button type="button" data-bs-target="#banner-indicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
 
     </div>
