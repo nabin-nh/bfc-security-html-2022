01252c4354ccdce5ef54c83cc49bf93ae9a6eedd
nabin-nh <nabin.hussain@gmail.com>
2022-04-18 18:46:09 +0100

    Finish product enquiry form styling

---
+++ parts/forms/product-enquiry.php
@@ -0,0 +1,63 @@
+<section class="product-enquiry py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 px-5">
+                <div class="inner">
+
+                    <h3 class="blue">Product Enquiry</h3>
+
+                    <form action="/" class="custom">
+                        <div class="row justify-content-center">
+
+                            <div class="form-group col-12">
+                                <select class="custom-select" name="select-product" id="select-product">
+                                    <option selected>Select product</option>
+                                    <option value="0">Lorem Ipsum</option>
+                                    <option value="1">Lorem Ipsum</option>
+                                    <option value="2">Lorem Ipsum</option>
+                                    <option value="3">Lorem Ipsum</option>
+                                    <option value="4">Lorem Ipsum</option>
+                                    <option value="5">Lorem Ipsum</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group col-12 col-lg-4 col-xl-6">
+                                <input type="text" placeholder="First name" class="form-control text" id="firstname" name="firstname">
+                            </div>
+
+                            <div class="form-group col-12 col-lg-4 col-xl-6">
+                                <input type="text" placeholder="Last name" class="form-control text" id="lastname" name="lastname">
+                            </div>
+
+                            <div class="form-group col-12 col-lg-4 col-xl-6">
+                                <input type="email" placeholder="Email address" class="form-control text" id="email" name="email">
+                            </div>
+
+                            <div class="form-group col-12 col-lg-4 col-xl-6">
+                                <input type="tel" placeholder="Phone number" class="form-control text" id="phone" name="phone">
+                            </div>
+
+                            <div class="form-group col-12 col-lg-4 col-xl-6">
+                                <input type="text" placeholder="Company" class="form-control text" id="company" name="company">
+                            </div>
+
+                            <div class="form-group col-12 col-lg-4 col-xl-6">
+                                <input type="tel" placeholder="Country" class="form-control text" id="country" name="country">
+                            </div>
+
+                            <div class="form-group col-12">
+                                <textarea id="message" name="message" placeholder="Type your message here...:" class="form-control form-control-lg no-resize" rows="6"></textarea>
+                            </div>
+
+                            <div class="button-group col-12 col-lg-8 col-xl-12">
+                                <button type="submit" class="btn blue custom text-uppercase my-xl-4">Send Enquiry</button>
+                            </div>
+
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
